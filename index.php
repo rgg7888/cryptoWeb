85615a3a607565f4d5154a0dd063f58b70254cbe
rgg7888 <rgg7888@outlook.com>
2021-04-11 15:18:14 -0600

    updating the structure

--- index.php
+++ index.php
@@ -7,9 +7,9 @@ html("Les",[
     head(null,[
         meta("CUTF-8"),
         cls(meta("Nviewport cwidth=device-width,_initial-scale=1.0")),
-        lk("Hhttps://fonts.googleapis.com/css2?family=
-        DM+Sans:wght@400;500;700&family=Inter:wght@300;500&display=swap Rstylesheet"),
-        cls(lk("Rshortcut_icon H./assets/icons/batata.svg"))
+        lk("Hhttps://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&family=Inter:wght@300;500&display=swap Rstylesheet"),
+        cls(lk("Rshortcut_icon H./assets/icons/batata.svg")),
+        title(null,"Batatabit")
     ]),
     body(null,[
 
